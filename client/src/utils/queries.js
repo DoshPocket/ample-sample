33b91e23dbd11e88820aecf13a0243c9d8167b2e
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-29 12:38:44 -0400

    server and config files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
-import { gql } from '@apollo/client';
+import  gql  from 'graphql-tag';
+
+export const GET_ME = gql `
+{
+  {
+    me {
+      _id
+      firstName
+      lastName
+      email
+      company
+      survey {
+        _id
+        title
+        description
+    }
+  }
+}
+`;
