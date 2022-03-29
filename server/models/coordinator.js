3ec9fa016ea1bfa9ae4fe08aaa5a60f0419d78cd
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-03-29 11:38:19 -0400

    added server side models

---
+++ server/models/coordinator.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const coordinatorSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    },
+  lastName: {
+    type: String,
+    required: true,
+    },
+  email: {
+    type: String,
+    default: 0,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+  password: {
+    type: String,
+    required: true,
+    len: [8],
+    },
+  company: {
+    type: String,
+    default: 0,
+  },
+});
+
+// set up pre-save middleware to create password
+coordinatorSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // compare the incoming password with the hashed password
+  coordinatorSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+const Coordinator = model('Coordinator', coordinatorSchema);
+
+module.exports = Coordinator;
