87dab62e90b263dcac0880384a1ecda4080f968f
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-30 09:53:36 -0400

    updating typedefs,mutations, resolvers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,64 +1,87 @@
 import gql from 'graphql-tag';
 
 // // boilerplate mutations
-// export const ADD_USER = gql`
-//   mutation addUser($username: String!, $email: String!, $password: String!) {
-//     addUser(username: $username, email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
+export const SIGN_UP = gql`
+  mutation Coordinator(firstname: String!, lastname: String!, email: String!, password: String!) {
+    Coordinator(firstname: firstname, lastname: lastname, email: email, password: password) {
+      token
+      Coordinator {
+        _id
+        firstname
+        lastname
+        email
+        company
+      }
+    }
+  }
+`;
 
-// export const LOGIN = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      Coordinator {
+        _id
+        email
+      }
+    }
+  }
+`;
 
-// export const SAVE_BOOK = gql`
-//   mutation saveBook($input: SavedBook!) {
-//     saveBook(input: $input) {
-//       _id
-//       username
-//       email
-//       bookCount
-//       savedBooks {
-//         bookId
-//         title
-//         authors
-//         image
-//         description
-//         link
-//       }
-//     }
-//   }
-// `;
+export const CREATE_SURVEY = gql`
+  mutation createSurvey(title: String!, description: String!, number_of_questions: Int!) {
+    createSurvey(title: String!, description: String!, number_of_questions: Int!) {
+      _id
+      title
+      description
+      number_of_questions 
+      questions {
+        _id
+        survey_id
+        question
+        answers
+      }
+    }
+  }
+`;
 
-// export const DELETE_BOOK = gql`
-//   mutation deleteBook($bookId: ID!) {
-//   deleteBook(bookId: $bookId) {
-//       _id
-//       username
-//       email
-//       bookCount
-//       savedBooks {
-//         bookId
-//         title
-//         authors
-//         image
-//         description
-//         link
-//       }
-//     }
-//   }
-// `;
+export const DELETE_SURVEY = gql`
+  mutation deleteSurvey($_id: ID!) {
+  deleteSurvey(id: $id) {
+      _id
+      title
+      description
+      number_of_questions 
+      questions {
+        _id
+        survey_id
+        question
+        answers
+      }
+    }
+  }
+`;
+
+export const CREATE_QUESTION = gql`
+  mutation createSurvey(title: String!, description: String!, number_of_questions: Int!) {
+    createSurvey(title: $title, description: description!, number_of_questions: number_of_questions!) {
+      _id
+      survey_id
+      question
+      answers
+      }
+    }
+  }
+`;
+
+export const DELETE_QUESTION = gql`
+  mutation deleteQuestion($_id: ID!) {
+  deleteQuestion(id: $id) {
+      _id
+      survey_id
+      question
+      answers
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,33 +3,38 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type Survey {
         _id: ID!
-        title: String!
-        description: String!
-        number_of_questions: Int!
+        title: String
+        description: String
+        number_of_questions: Int
         questions: [Question]
     }
 
     type Question {
         _id: ID!
-        survey: ID!
-        question: String!
+        survey: ID
+        question: String
+        answers: [answer]
+        `
+        //answers: [answer] not sure if this is needed?
+        `
     }
 
     type Coordinator {
         _id: ID!
-        firstname: String!
-        lastname: String!
-        email: String!
-        password: String!
-        comapny: String
+        firstname: String
+        lastname: String
+        email: String
+        password: String
+        company: String
+        surveys: [survey]
     }
     type Mutation {
-        createSurvey(title: String!, description: String!, number_of_questions: Int!): Survey
-        createQuestion(survey: ID!, question: String!): Question
+        createSurvey(title: String!, description: String!, number_of_questions: Int!): Coordinator
+        createQuestion( question: String!, answers: String!): Survey
         login(email: String!, password: String!): Auth
-        signup(username: String!, email: String!, password: String!): Auth
-        deleteSurvey(survey: ID!): Survey
-        deleteQuestion(question: ID!): Question
+        signup(firstname: String!, lastname: String!, email: String!, password: String!): Auth
+        deleteSurvey(_id: ID!): Coordinator
+        deleteQuestion(_id: ID!): Survey
     }
     type Query {
         me: Coordinator
@@ -38,11 +43,12 @@ const typeDefs = gql`
       }
 
     type Auth {
-        token: String!
-        user: Coordinator!
+        token: ID!
+        coordinator: Coordinator
     }
 
 `;
 
 module.exports = typeDefs;
 
+// Currently only query'ing 'me' for authenticated 'users' (aka Coordinators);
