c79fdc53ba6704a22018cdb97e1576b64a210186
Brandon Winkler <brandonwinkler@live.ca>
2022-03-29 21:17:48 -0400

    fixed issue not finding home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
+import Home from './pages/home.js';
 // import NotFound from './pages/NotFound';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
