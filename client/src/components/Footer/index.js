f00279bd52414ab0d222fa95a878759e801639fa
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-03-31 15:09:21 -0400

    added remaining routes to app.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,6 +4,7 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 
+
 const theme = createTheme({
 
     palette: {
